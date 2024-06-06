e7cc0d4ef8f4d385fdda61f091f7857f3207bde3
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-06-06 22:51:26 +0200

    lazy loading for product details implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import ProductDetails from "./pages/ProductDetails/ProductDetails";
+
+// Lazy load the ProductDetails component
+const ProductDetails = lazy(() =>
+  import("./pages/ProductDetails/ProductDetails")
+);
 
 function App() {
   return (
@@ -13,10 +17,12 @@ function App() {
           <Navbar />
         </header>
         <div className="main-container">
-          <Routes>
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/" element={<Navigate to="/product/1" />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/product/:id" element={<ProductDetails />} />
+              <Route path="/" element={<Navigate to="/product/1" />} />
+            </Routes>
+          </Suspense>
           <Footer />
         </div>
       </div>
