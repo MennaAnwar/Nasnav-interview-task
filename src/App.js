ffe3b657ba1be1ca6f974610e02526d26d2b15ba
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-06-06 13:25:17 +0200

    static images added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.scss";
+import Product from "./pages/ProductDetails/Product";
 
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <h1 className="app-title">Welcome to React</h1>
-      </header>
-      <p className="app-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <Product />
     </div>
   );
 }
